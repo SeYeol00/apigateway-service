bce72f8092f4e801d63bdc2b40e7514825e2535a
SeYeol00 <dmot0126@gmail.com>
2023-02-25 20:40:19 +0900

    Update CustomFilter.java

--- src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
+++ src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -21,7 +21,7 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
     }
 
     @Override
-    public GatewayFilter apply(Object config) {
+    public GatewayFilter apply(Config config) {
         // 커스텀 pre 필터로 JWT토큰 추출과 인증을 관리할 수 있다.
         return ((exchange, chain) -> {
             // 네티라는 비동기 서버를 사용할 때는 아래의 리퀘스트와 리스폰스를 사용한다.
@@ -32,10 +32,11 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
             // 커스텀 post 필터로 에러 리스폰스 핸들러를 에러 코드에 따라 부를 수 있다.
             // 아래 리턴 객체로 등록이 가능하다.
-            // 스프링 5에서 지원되는 웹플럭스 기술로 반환타입을 전달 가능하다.
+            // 모노는 스프링 5에서 지원되는 웹플럭스 기술로 비동기 방식으로 단일값을 전달 가능하다.
             return chain.filter(exchange).then(Mono.fromRunnable(()-> {
                 log.info("커스텀 post 필터: response code = {}", response.getStatusCode());
             }));
         });
     }
+
 }
